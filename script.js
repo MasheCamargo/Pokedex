a6d46da9adf5efac2bc62bbc79653c46f77e1ba4
Mashe.Camargo <winny.camargo@gmail.com>
2023-07-11 01:00:29 -0500

    commit2

--- script.js
+++ script.js
@@ -1,29 +1,50 @@
+const baseURL = "https://pokeapi.co/api/v2/pokemon/";
+const pokeContent = document.getElementById('PokeName');
+const buttonSearch = document.getElementById('searchPokemon');
+const removePokemon = document.getElementById('borrarPokemon');
+const appNode = document.getElementById('app');
 
-const  baseURL= "https://pokeapi.co/api/v2/pokemon/"
-const pokeContent = document.getElementById('PokeName')
-const buttonSearch = document.getElementById('searchPokemon')
-const appNode = document.getElementById ('app')
+buttonSearch.addEventListener('click', insertarPokemon);
+removePokemon.addEventListener('click', eliminarPokemones);
 
-buttonSearch.addEventListener('click', insertarPokemon)
-
-function insertarPokemon(){
+function insertarPokemon() {
     window.fetch(`${baseURL}${pokeContent.value.toLowerCase()}`)
-    .then(response =>{
-        if(response.status===404){
-            alert('Este pokemon no está disponible')
-        }else {
-            return response.json()
-        }
-    })
+        .then(response => {
+            if (response.status === 404) {
+                alert('Este pokemon no está disponible');
+            } else {
+                return response.json();
+            }
+        })
         .then(responseJSON => {
-        const allItems =[]
-        const result = []
+            const result = [];
 
-        for(let pokemonInfo in responseJSON){
-            result.push([pokemonInfo, responseJSON[pokemonInfo]])
-        }
-        console.table(result)        
-    })
+            for (let pokemonInfo in responseJSON) {
+                result.push([pokemonInfo, responseJSON[pokemonInfo]]);
+            }
+            console.table(result);
+
+            const pokeImagen = document.createElement('img');
+            pokeImagen.src = responseJSON.sprites.front_default;
+
+            const pokemonName = document.createElement('h1');
+            pokemonName.innerText = `Name: ${responseJSON.name} | id: ${responseJSON.id}`;
+
+            const pokemonType = document.createElement('h3');
+            pokemonType.innerText = `Type: ${responseJSON.types[0].type.name}`;
+
+            const contenedor = document.createElement('section');
+            contenedor.append(pokeImagen, pokemonName, pokemonType);
+            
+            appNode.appendChild(contenedor);
+        })
+        .catch(error => {
+            console.log('Error:', error);
+        });
+}
+
+function eliminarPokemones() {
+    appNode.innerHTML = ''; // Limpiar contenido
 }
 
 
